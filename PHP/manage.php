bc268a055e2389db9380ad8e6a8126b60f29396b
Bekione <112067722+Bekione@users.noreply.github.com>
2023-05-21 23:14:13 +0300

    Update manage.php

--- PHP/manage.php
+++ PHP/manage.php
@@ -7,7 +7,7 @@ include 'connect.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="manage.css?v=<?php echo time() ?>">
+    <link rel="stylesheet" href="./Css/manage.css?v=<?php echo time() ?>">
     <link rel="shortcut icon" href="Assets/logo.png">
     <title>Manage Pc Owners</title>
 </head>
